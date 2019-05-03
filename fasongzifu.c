2094393a9a55889b15156d14764c237c0e0872a5
LeoMachine <1962976634@qq.com>
2019-05-03 13:04:36 +0800

    Initial commit

---
+++ fasongzifu.c
@@ -0,0 +1,52 @@
+/*
+ * 单片机发送两个字符给PC机 波特率 9600Mhz SMOD=0 fosc=11.0592Mhz
+ * (SMOD=1 时 PCON=0X80 要写上)
+ *计算初值
+ *波特率=（2^SMOD/32）*(fosc/((256-X)*12))
+ *
+ * */
+#include "reg52.h"
+#define  uc unsigned  char
+//初始化串口
+void init()
+{
+
+    TMOD=0x20;//用定时器1 方式2 自动重装初值 M1=1 M0=0
+    TH1=253;//计算初值  波特率9600bps时
+    TL1=253;//低八位可以不设置
+    TR1=1;//启动定时器T1
+
+    SM0=0;//使用SCON方式一
+    SM1=1;
+    REN=1;//启动串口接收数据
+
+    //SCON结构 SM1 SM1 SM2 REN TB8 RB8 TI RI
+    //也可以 SCON=0X50
+
+    //使用查询方式
+
+}
+void sendbyte(uc data)
+{
+   //发送单个字符给PC机
+   SBUF=data;
+   while (!TI);//TI=0 时一直等待
+   TI=0;//手动将发送中断标志位清零
+}
+void sendstr(uc *s)
+{
+    while (*s!='\0')
+    {
+        sendbyte(*s);
+        s++;
+    }
+}
+
+void main()
+{
+    init();
+    sendstr("2个字符");
+    while (1);
+}
+
+
---
+++ serial.c
@@ -0,0 +1,43 @@
+/*
+ * 单片机接收一个数据，用LED验证接收到的数据 波特率 4800 SMOD=1  fosc=11.0592Mhz
+ *计算初值
+ *波特率=（2^SMOD/32）*(fosc/((256-X)*12))
+ *
+ * */
+#include "reg52.h"
+//初始化串口
+void init()
+{
+ //SMOD=1 时 PCON=0X80 要写上
+
+    TMOD=0x20;//用定时器1 方式2 自动重装初值 M1=1 M0=0
+    TH1=244;//计算初值  波特率4800bps时
+    TL1=244;//低八位可以不设置
+    TR1=1;//启动定时器T1
+
+    SM0=0;//使用SCON方式一
+    SM1=1;
+    REN=1;//启动串口接收数据
+
+    //SCON结构 SM1 SM1 SM2 REN TB8 RB8 TI RI
+    //也可以 SCON=0X50
+    //中断方式
+    EA=1;
+    ES=1;
+}
+void main()
+{
+    init();
+    while(1);
+}
+void serial() interrupt 4
+{
+    unsigned  char temp;
+    if(RI)
+{
+    RI=0;//将接收中断标志位 手动清零
+    temp=SBUF;//将SBUF中的数据取出来
+    P1=temp;
+
+}
+}
